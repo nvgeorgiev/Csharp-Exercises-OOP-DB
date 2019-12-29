234c495ec580fb959dc35360fd328522fa35cfcf
Nikolay Georgiev <nv_georgiev@abv.bg>
2019-12-29 17:16:15 +0200

    Add files via upload
    Final-Exam-creating-of-database

---
+++ DB/EntityFrameworkCore/Exam-07-12-2019/TeisterMask/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace TeisterMask.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString = @"Server=DESKTOP-NN6P8LO\SQLEXPRESS01;Database=TeisterMask;Trusted_Connection=True";
+    }
+}
